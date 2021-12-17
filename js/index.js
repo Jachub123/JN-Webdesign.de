a4e1f2d7051b60e2e4693ec0e72db2b0a07d9d73
Jakob <jakobnonnenmacher@hotmail.com>
2021-12-17 02:10:56 +0100

    reverted

--- js/index.js
+++ js/index.js
@@ -141,15 +141,6 @@ class Header extends React.Component {
             <div className="CenterHeaderNav">
               <div className="headerNavContainer">
                 <nav className=" header">
-                  <div
-                    className="drop-shadow burgerMenu"
-                    onClick={() => this.hover()}
-                  >
-                    <div className="line line1"></div>
-                    <div className="line line2"></div>
-                    <div className="line line3"></div>
-                    <div className="line line4"></div>
-                  </div>
                   <ul>
                     <li className="linkLiElement">
                       <a href="#">
@@ -168,6 +159,15 @@ class Header extends React.Component {
                     </li>
                   </ul>
                 </nav>
+                <div
+                  className="drop-shadow burgerMenu"
+                  onClick={() => this.hover()}
+                >
+                  <div className="line line1"></div>
+                  <div className="line line2"></div>
+                  <div className="line line3"></div>
+                  <div className="line line4"></div>
+                </div>
               </div>
             </div>
           </div>
