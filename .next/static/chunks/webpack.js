9fb4627d36b1e477d2063b4d4d02bae61fd8d22d
Jakob <jakobnonnenmacher@hotmail.com>
2022-04-06 21:50:05 +0200

    created classes for animations and added a function in animations.js to apply those classes when elements are scrolled into view

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -128,7 +128,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "b0556488f597df87"; }
+/******/ 		__webpack_require__.h = function() { return "a465583e10ecb955"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
